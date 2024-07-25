f8a2b74d956c35def0c1e74fcc060d6d9e55e59f
noobmaster122 <brahimi.a.farouk@gmail.com>
2024-07-25 14:41:31 +0200

    added syncfusion key

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NCaF1cWWhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEFjXn1bcHxUR2JdUU1/Ww==');
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
