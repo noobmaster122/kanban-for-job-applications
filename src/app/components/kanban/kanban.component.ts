0de703dd09d2f768b6b5e69a4bd5bf22866b2162
noobmaster122 <brahimi.a.farouk@gmail.com>
2024-08-01 21:34:06 +0200

    refactored kanban datasource observable

--- src/app/components/kanban/kanban.component.ts
+++ src/app/components/kanban/kanban.component.ts
@@ -1,55 +1,46 @@
-import { AfterViewChecked, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, ChangeDetectorRef, Component, OnDestroy, ViewChild } from '@angular/core';
 import { CardSettingsModel, ColumnsModel } from '@syncfusion/ej2-angular-kanban';
 import { KanbanHandlerService } from 'src/app/services/kanban-handler.service';
 import { Subscription } from 'rxjs';
 import { KanbanModalComponent } from '../kanban-modal/kanban-modal.component';
-import { columns as columnsData } from 'src/app/services/data';
+import { Card } from 'src/app/models/card.model';
 
 @Component({
   selector: 'app-kanban',
   styleUrls: ['./kanban.component.css'],
   template: `
-  <ejs-kanban class='kanban-custom' [dataSource]='data' [columns]='columns' keyField="status" [cardSettings]='cardSettings'>         
-  <ng-template #cardSettingsTemplate let-data> 
+  <ejs-kanban class='kanban-custom' height="400" width="100%" [dataSource]='data' [columns]='columns' keyField="status" [cardSettings]='cardSettings' enableTooltip='true'>         
+    <ng-template #cardSettingsTemplate let-data> 
       <div class="card-template"> 
-          <div class="e-card-header">
-            <h4>{{ data.positionTitle }}</h4>
-          </div>
-          <div class="e-card-content">
-            <p><strong>LinkedIn:</strong> <a [href]="data.hiringManagerLinkedIn" target="_blank">{{ data.hiringManagerName }}</a></p>
-            <p><strong>Contacted:</strong> {{ data.haveContactedHiringManager ? 'Yes' : 'No' }}</p>
-          </div> 
-        </div> 
+        <span>{{ data.positionTitle | truncate:8 }}</span>
+        <span  [ngClass]="{'avatar-contacted': data.haveContactedHiringManager, 'avatar-not-contacted': !data.haveContactedHiringManager}"
+        class="avatar"><a [href]="data.hiringManagerLinkedIn" target="_blank">🧑</a></span>
+      </div> 
     </ng-template> 
+    <ng-template #tooltipTemplate let-data>
+        <div class='e-kanbanTooltipTemp'>
+            hello world
+        </div>
+    </ng-template>
   </ejs-kanban>
   <app-kanban-modal #modal></app-kanban-modal>
   `
 })
-export class KanbanComponent implements AfterViewChecked, OnInit, OnDestroy {
+export class KanbanComponent implements AfterViewChecked, OnDestroy {
   @ViewChild('modal') modal!: KanbanModalComponent;
   
-  public columns!: ColumnsModel[];
-  public data!: object[];
-  public cardSettings!: CardSettingsModel;
-  private subscription: Subscription | null = null;
+  public columns: ColumnsModel[] = [];
+  public data: Card[] = [];
+  public cardSettings: CardSettingsModel;
+  private subscription: Subscription = new Subscription();
 
   constructor(private kanbanService: KanbanHandlerService, private cdr: ChangeDetectorRef) {
-  }
-
-  ngOnInit() {
     this.columns = this.kanbanService.columns;
-
-    this.subscription = this.kanbanService.cardStore.subscribe((data: object[]) => {
-      this.data = data;
-    });
-
-  }
-
-  ngAfterViewInit(){
-    this.data = this.kanbanService.data;
     this.cardSettings = this.kanbanService.cardSettings;
-    console.log("am data", this.data);
-
+    this.subscription.add(this.kanbanService.cardStore$.subscribe((data: Card[]) => {
+      this.data = data;
+      console.log("Data updated:", data);
+    }));
   }
 
   openDialog(keyField: string) {
@@ -57,19 +48,15 @@ export class KanbanComponent implements AfterViewChecked, OnInit, OnDestroy {
   }
 
   ngAfterViewChecked(): void {
-    this.cdr.detectChanges();
     const emptyCards = document.querySelectorAll('.e-empty-card');
     emptyCards.forEach((element) => {
       (element as HTMLElement).style.display = 'none';
     });
 
-    //this causes an infinite loop
-    //only do this logic if no triggers are found
     const triggers = document.querySelectorAll('.open-dialog-trigger');
     triggers.forEach(trigger => {
       trigger.addEventListener('click', (event) => {
         const keyField = (event.currentTarget as HTMLElement).getAttribute('data-key');
-
         this.openDialog(keyField || 'toApplyStatus');
       });
     });
--- src/app/services/kanban-handler.service.ts
+++ src/app/services/kanban-handler.service.ts
@@ -1,6 +1,6 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { data,columns,cardSettings } from './data';
+import { data, columns, cardSettings } from './data';
 import { CardSettingsModel, ColumnsModel } from '@syncfusion/ej2-angular-kanban';
 import { Card } from '../models/card.model';
 
@@ -8,25 +8,14 @@ import { Card } from '../models/card.model';
   providedIn: 'root'
 })
 export class KanbanHandlerService {
-  public cardStore = new EventEmitter<object[]>();
+  private cardStoreSubject = new BehaviorSubject<Card[]>(data);
+  public cardStore$ = this.cardStoreSubject.asObservable();
   public columns: ColumnsModel[] = columns;
-  public data: Card[] = data;
   public cardSettings: CardSettingsModel = cardSettings;
 
 
-  constructor() {
-    this.updateKanbanCards();
+  public addCard(newCard: Card) {
+    const updatedData = [...this.cardStoreSubject.getValue(), newCard];
+    this.cardStoreSubject.next(updatedData);
   }
-
-  private updateKanbanCards(){
-    this.cardStore.emit(this.data);
-  }
-
-  // Add a new card
-  addCard(newCard: Card) {
-    this.data = [...this.data, newCard];
-    this.updateKanbanCards();
-  }
-
-  // Additional methods for updating or removing cards
 }
